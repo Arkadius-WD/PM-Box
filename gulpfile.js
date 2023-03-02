8017bd3b0a86c74d135b2a2e7d85233cb6d9d8ec
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-03-02 14:07:47 +0100

    gulp start

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+import { task } from 'gulp'
+
+task('css', function (done) {
+	console.log('zadanie CSSa')
+	done()
+})
+
+task('default', function (done) {
+	console.log('domyslene zadanie')
+	done()
+})
