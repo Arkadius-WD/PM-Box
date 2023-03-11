8dc5db078ec8fd5fcd89c8262de8bb299e6b70d4
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-03-11 11:56:35 +0100

    config vite

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.scss'
+import javascriptLogo from './src/javascript.svg'
+import { setupCounter } from './src/counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,5 +19,4 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
 setupCounter(document.querySelector('#counter'))
