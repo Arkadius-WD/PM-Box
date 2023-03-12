8a487492feb964cfefc588a69d802c19c3d7b696
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-03-12 13:50:42 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
-import sass from 'sass';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        implementation: sass,
-      },
-    },
-  },
+  }
 });
