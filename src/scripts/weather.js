2f97b2f4f4a36b9cb44245eeb156529646b6f2fe
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-04-02 13:18:07 +0200

    weather modifi

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,56 +1,46 @@
 import axios from 'axios';
 
 const weather = () => {
-  function date() {
-    const currentDate = new Date();
-    const options = {
-      month: 'short',
-      day: 'numeric',
-    };
-    const tomorrowDate = new Date();
-    tomorrowDate.setDate(currentDate.getDate() + 1);
-    const afterTomorrowDate = new Date();
-    afterTomorrowDate.setDate(currentDate.getDate() + 2);
+  const daysContainer = document.querySelector('.weather__days');
+  const inputCity = document.querySelector('.weather__city-input');
 
-    const formattedCurrentDate = currentDate.toLocaleDateString(
-      'en-US',
-      options,
-    );
-    const formattedTomorrowDate = tomorrowDate.toLocaleDateString(
-      'en-US',
-      options,
-    );
-    const formattedAfterTomorrowDate = afterTomorrowDate.toLocaleDateString(
-      'en-US',
-      options,
-    );
-
-    document.querySelector('.text-today').textContent = formattedCurrentDate;
-    document.querySelector('.text-tomorrow').textContent =
-      formattedTomorrowDate;
-    document.querySelector('.text-after').textContent =
-      formattedAfterTomorrowDate;
-  }
-
-  const forecast = () => {
-    const inputCuty = document.querySelector('.weather__city-input');
-
-    const API_LINK = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-    const API_KEY = '&appid=8ce19327be3e6730718f881dd238de8b';
-    const API_UNITS = '&units=metric';
-
-    const getWeather = () => {
-      const city = inputCuty.value || 'London';
-      const URL = API_LINK + city + API_KEY + API_UNITS;
-
-      axios.get(URL).then(res => console.log(res));
-    };
-
-    getWeather();
+  ///  DATE ///
+  const currentDate = new Date();
+  const nextDate = new Date(currentDate);
+  const options = {
+    month: 'short',
+    day: 'numeric',
   };
 
-  date();
-  forecast();
+  /// API ///
+  const API_LINK = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+  const API_KEY = '&appid=8ce19327be3e6730718f881dd238de8b';
+  const API_UNITS = '&units=metric';
+
+  /// GET WEATHER ///
+  const city = inputCity.value || 'London';
+  const URL = API_LINK + city + API_KEY + API_UNITS;
+  axios.get(URL).then(res => console.log(res));
+
+  /// CREATE ELEMENTS ///
+  for (let index = 0; index < 5; index++) {
+    nextDate.setDate(currentDate.getDate() + index);
+    const formattedCurrentDate = nextDate.toLocaleDateString('en-US', options);
+
+    const div = document.createElement('div');
+    div.classList.add('weather__day');
+    div.setAttribute('id', `weather__day-${index}`);
+
+    div.innerHTML = `
+      <img src="./img/weather/sun.png" alt="weather icon" />
+      <div class="weather__day-temperature">
+        <p>7℃/5℃</p>
+      </div>
+      <p class="text-day">${formattedCurrentDate}</p>
+    `;
+
+    daysContainer.appendChild(div);
+  }
 };
 
 export default weather;
