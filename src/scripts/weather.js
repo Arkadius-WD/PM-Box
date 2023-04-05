c061c370e2c897378c663605d7ae7a82927ece08
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-04-05 16:13:39 +0200

    icons update

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
-import sunIcon from '../img/weather/sun.png';
+import Clouds from '../img/weather/Clouds.png';
+import Drizzle from '../img/weather/Drizzle.png';
+import Snow from '../img/weather/Snow.png';
+import Rain from '../img/weather/Rain.png';
+import Clear from '../img/weather/Clear.png';
+import Thunderstorm from '../img/weather/Thunderstorm.png';
+
+const weatherIcons = {
+  Clear,
+  Clouds,
+  Drizzle,
+  Snow,
+  Rain,
+  Thunderstorm,
+};
 
 const weather = () => {
   const daysContainer = document.querySelector('.weather__days');
@@ -23,7 +37,9 @@ const weather = () => {
   const URL = API_LINK + city + API_KEY + API_UNITS;
 
   axios.get(URL).then(res => {
-    /// CREATE ELEMENTS ///
+    console.log(res.data.list[0].weather);
+
+    /// Create elements ///
     for (let index = 0; index < 5; index++) {
       nextDate.setDate(currentDate.getDate() + index);
       const formattedCurrentDate = nextDate.toLocaleDateString(
@@ -31,13 +47,16 @@ const weather = () => {
         options,
       );
       const temp = Math.floor(res.data.list[index * 8].main.temp);
+      const status = { ...res.data.list[index * 8].weather[0] };
+
+      console.log(status.main);
 
       const div = document.createElement('div');
       div.classList.add('weather__day');
       div.setAttribute('id', `weather__day-${index}`);
       div.innerHTML = `
       <p class="text-day">${formattedCurrentDate}</p>
-      <img src="${sunIcon}" alt="weather icon" />
+      <img src="${weatherIcons[status.main]}" alt="weather icon" />
       <div class="weather__day-temperature">
       <p class="temp">${temp}°C</p>
       </div>
