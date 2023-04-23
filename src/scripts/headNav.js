93d5bdb1b9c63b53f11a88c0be935ee03086a795
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-04-23 17:18:48 +0200

    js show menu settings

--- src/scripts/headNav.js
+++ src/scripts/headNav.js
@@ -4,6 +4,10 @@ const headNav = () => {
   const talks = document.querySelector('.talks');
   const envelopeInside = document.querySelector('.envelope__inside');
   const mainContainer = document.querySelector('.main-container');
+  const menuSettings = document.querySelector('.nav__menu-settings');
+  const navSettingsIcon = document.querySelector('.nav__settings');
+  const upperBeamSettings = document.querySelector('.upper-beam__settings');
+  const adminMenu = document.querySelector('.upper-beam__admin-menu');
 
   const showMenu = () => {
     lowerBeam.classList.toggle('beam-hidden');
@@ -14,9 +18,18 @@ const headNav = () => {
     talks.classList.toggle('talks-hidden');
   };
 
-  logo.addEventListener('click', showMenu);
+  const showMenuSettings = () => {
+    menuSettings.classList.toggle('menu-settings-active');
+  };
 
+  const showAdminMenu = () => {
+    upperBeamSettings.classList.toggle('admin-menu-active');
+  };
+
+  logo.addEventListener('click', showMenu);
   envelopeInside.addEventListener('click', showTalks);
+  navSettingsIcon.addEventListener('click', showMenuSettings);
+  adminMenu.addEventListener('click', showAdminMenu);
 };
 
 export default headNav;
