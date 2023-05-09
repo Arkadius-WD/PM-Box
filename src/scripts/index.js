6d1e6efbfaac1ff6f731c4384c533995cf228916
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-05-09 21:46:16 +0200

    index.js actualisation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,32 +5,30 @@ import headNav from './headNav';
 import weather from './weather';
 import schedule from './schedule';
 
-if (
-  window.location.pathname.endsWith('/') ||
-  window.location.pathname.endsWith('/index.html')
-) {
+if (['/', '/index.html'].includes(window.location.pathname)) {
   login();
 }
 
 if (
-  window.location.pathname.endsWith('/home.html') ||
-  window.location.pathname.endsWith('/schedule.html') ||
-  window.location.pathname.endsWith('/todo.html') ||
-  window.location.pathname.endsWith('/portfolio.html') ||
-  window.location.pathname.endsWith('/approvals.html') ||
-  window.location.pathname.endsWith('/budget.html') ||
-  window.location.pathname.endsWith('/files.html') ||
-  window.location.pathname.endsWith('/teams.html') ||
-  window.location.pathname.endsWith('/history.html') ||
-  window.location.pathname.endsWith('/git.html')
+  [
+    '/home.html',
+    '/schedule.html',
+    '/todo.html',
+    '/portfolio.html',
+    '/approvals.html',
+    '/budget.html',
+    '/files.html',
+    '/teams.html',
+    '/history.html',
+    '/git.html',
+  ].includes(window.location.pathname)
 ) {
   headNav();
 }
 
-if (window.location.pathname.endsWith('/home.html')) {
+if ('/home.html'.includes(window.location.pathname)) {
   weather();
 }
-
-if (window.location.pathname.endsWith('/schedule.html')) {
+if ('/schedule.html'.includes(window.location.pathname)) {
   schedule();
 }
