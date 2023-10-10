97a9795297dfdabab20128be36b56a3f030a5810
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-10-10 10:09:05 +0200

    messages windows modification on portfolio

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -2,15 +2,39 @@ const portfolio = () => {
   const buttonApprovals = document.querySelector('[data-id="12"]');
   const buttonTodo = document.querySelector('[data-id="17"]');
   const buttonMessages = document.querySelector('[data-id="34"]');
+   const approvalWindow = document.querySelector('.notify-1');
+  const todoWindow = document.querySelector('.notify-2');
+  const messagesWindow = document.querySelector('.notify-3');
+  const closeButton1 = document.querySelector(
+    '.close-button[data-window="notify-1"]',
+  );
+  const closeButton2 = document.querySelector(
+    '.close-button[data-window="notify-2"]',
+  );
+  const closeButton3 = document.querySelector(
+    '.close-button[data-window="notify-3"]',
+  );
 
   buttonApprovals.addEventListener('click', () => {
-    console.log('Button Approve');
+    approvalWindow.classList.toggle('hidden');
   });
+
   buttonTodo.addEventListener('click', () => {
-    console.log('Button Todo');
+    todoWindow.classList.toggle('hidden');
   });
+
   buttonMessages.addEventListener('click', () => {
-    console.log('Button Messages');
+    messagesWindow.classList.toggle('hidden');
+  });
+
+  closeButton1.addEventListener('click', () => {
+    approvalWindow.classList.toggle('hidden');
+  });
+  closeButton2.addEventListener('click', () => {
+    todoWindow.classList.toggle('hidden');
+  });
+  closeButton3.addEventListener('click', () => {
+    messagesWindow.classList.toggle('hidden');
   });
 };
 export default portfolio;
