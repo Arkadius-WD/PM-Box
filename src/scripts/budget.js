f12947cf8f406b0ae16cb006af19c31abd920f36
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2023-10-20 12:51:54 +0200

    display change project

--- src/scripts/budget.js
+++ src/scripts/budget.js
@@ -1,24 +1,24 @@
 const budget = () => {
-  const account1 = {
-    title: 'Project #00015',
-    movements: [4500, -1000, -130, -100, 200, -50, 100],
+  const project00015 = {
+    number: '#00015',
+    movements: [50000, -10000, -11200.5, -1000, 20000, -5000, 10000],
   };
 
-  const account2 = {
-    title: 'Project #00013',
-    movements: [5000, -3400, -150, -30],
+  const project00013 = {
+    number: '#00013',
+    movements: [150000, -34000, -15000, -30000],
   };
 
-  const account3 = {
-    title: 'Project #00011',
-    movements: [200, -20, -80, -100, 50],
+  const project00011 = {
+    number: '#00011',
+    movements: [20000, -2000, -8000, -10000],
   };
-  const account4 = {
-    title: 'Project #00010',
-    movements: [2000, -200, 340, -300, -20, -460],
+  const project00010 = {
+    number: '#00010',
+    movements: [25000, -20000, 3400, -10000, -2000, -4600],
   };
 
-  const accounts = [account1, account2, account3, account4];
+  const accounts = [project00015, project00013, project00011, project00010];
 
   const currencies = new Map([
     ['USD', 'United States dollar'],
@@ -37,7 +37,9 @@ const budget = () => {
   const containerApp = document.querySelector('.app');
   const containerMovements = document.querySelector('.movements');
 
-  const btnLogin = document.querySelector('.login__btn');
+  const inputChangeProject = document.querySelector('.input__change-project');
+  const inputSelectProject = document.getElementById('change-project');
+
   const btnTransfer = document.querySelector('.form__btn--transfer');
   const btnLoan = document.querySelector('.form__btn--loan');
   const btnClose = document.querySelector('.form__btn--close');
@@ -50,6 +52,8 @@ const budget = () => {
   const inputLoanAmount = document.querySelector('.form__input--loan-amount');
   const inputCloseUsername = document.querySelector('.form__input--user');
   const inputClosePin = document.querySelector('.form__input--pin');
+
+  let currentAccount = project00015;
 
   const displayMovements = movements => {
     containerMovements.innerHTML = '';
@@ -72,7 +76,6 @@ const budget = () => {
     const html2 = `<button class="btn--sort">&downarrow; SORT</button>`;
     containerMovements.insertAdjacentHTML('beforeend', html2);
   };
-  displayMovements(account1.movements);
 
   let balance = 0;
   let incomes = 0;
@@ -82,7 +85,6 @@ const budget = () => {
     balance = movements.reduce((acc, cur) => acc + cur, 0);
     labelBalance.textContent = `${balance} €`;
   };
-  calcDisplayBalance(account1.movements);
 
   const calcDisplaySummary = movements => {
     incomes = movements
@@ -95,22 +97,40 @@ const budget = () => {
     );
     labelSumOut.textContent = `${out} €`;
   };
-  calcDisplaySummary(account1.movements);
 
   const calcRest = () => {
     const rest = (((incomes - out) / incomes) * 100).toFixed(1);
     labelSumInterest.textContent = `${rest} %`;
   };
+
+  displayMovements(currentAccount.movements);
+  calcDisplayBalance(currentAccount.movements);
+  calcDisplaySummary(currentAccount.movements);
   calcRest();
 
   const createNumberProject = accs => {
     accs.forEach(acc => {
-      const projectNumber = acc.title.split(' ');
+      const projectNumber = acc.number.split(' ');
       const lastWord = projectNumber[projectNumber.length - 1];
       acc.project = lastWord.slice(-6);
     });
   };
   createNumberProject(accounts);
+
+  inputChangeProject.addEventListener('click', e => {
+    e.preventDefault();
+    const selectedOption =
+      inputSelectProject.options[inputSelectProject.selectedIndex];
+
+    currentAccount = accounts.find(acc => acc.project === selectedOption.value);
+
+    if (currentAccount.number === selectedOption.value) {
+      displayMovements(currentAccount.movements);
+      calcDisplayBalance(currentAccount.movements);
+      calcDisplaySummary(currentAccount.movements);
+      calcRest();
+    }
+  });
 };
 
 export default budget;
